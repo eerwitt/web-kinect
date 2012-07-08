7b2f9685dd1c0bb39a3b9a42a17589d28dc4813f
Erik Erwitt <eerwitt@gmail.com>
2012-07-08 15:53:41 -0700

    tried lzma over websockets but need to switch to different web framework

--- web/app/actions/echo_action.rb
+++ web/app/actions/echo_action.rb
@@ -1,16 +1,19 @@
 require 'rack/websocket'
+require 'lzma'
+
+STDOUT.sync = true
 
 class EchoAction <  Rack::WebSocket::Application
   def on_open(env)
     puts "Client connected"
-    EM.next_tick do
-      @sub = EM::Hiredis.connect("redis://localhost:6379")
+    @sub = EM::Hiredis.connect("redis://localhost:6379")
+    @sub.subscribe "kinect_raw"
 
-      @sub.subscribe "kinect_raw"
+    EM.next_tick do
       # {:points => [{:x => 123, :y => 234, :z => 456}]}
       @sub.on(:message) do |channel,pixels| 
         puts "Got pixels #{pixels.length}"
-        send_data pixels
+        send_data LZMA.decompress(pixels)
       end
     end
   end
